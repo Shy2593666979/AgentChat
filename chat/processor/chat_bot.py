c24f1602cc9e71d9a666cd1237ed3f37db16e8a0
Shy2593666979 <2593666979@qq.com>
2024-08-26 17:41:55 +0800

    Update chat_bot.py

--- chat/processor/chat_bot.py
+++ chat/processor/chat_bot.py
@@ -1,6 +1,6 @@
 from typing import List
 from type.message import Message
-from chat.llm.openai import LLMChat
+from llm.openai import LLMChat
 from prompt.template import function_call_template, ask_user_template, action_template
 from prompt.llm_prompt import fail_action_prompt
 from langchain.prompts import PromptTemplate
@@ -12,7 +12,8 @@ from loguru import  logger
 class ChatbotModel:
     def __init__(self):
         self.historyMessage: List[Message] = []
-
+        self.final_result: str = ''
+    
     def include_history_message(self, message: Message):
         """try history message include this ChatbotModel class"""
         self.historyMessage.append(message)
@@ -22,7 +23,7 @@ class ChatbotModel:
 
     async def run(self, user_input: str, historyMessage: List[Message], agent: str):
 
-        breakpoint()
+        # breakpoint()
         function = Agent.get_parameter_by_name(agent)
 
         prompt_template = PromptTemplate.from_template(function_call_template)
@@ -32,32 +33,39 @@ class ChatbotModel:
             prompt_history = prompt_history + msg.to_str()
 
         final_prompt = prompt_template.format(user_input=user_input, history=prompt_history)
-        function_name, function_args = LLMChat.llm_function_call(final_prompt, function)
+        function_name, function_args = await LLMChat.llm_function_call(final_prompt, function)
 
         # 直接与模型对话，不调用function
         if function_name is None or function_name not in action_class:
-            yield LLMChat.llm_chat(function_call_template, user_input=user_input, history=prompt_history)
-
-        if BotCheck.slot_is_full(function_args, function_name):
-            logger.info("parameters is full !")
-            # 判断是否是自定义Agent
-            if Agent.check_name_iscustom(function_name):
-                action_result = self.custom_action(function_name, function_args)
-            else:
-                action_result = self.action(function_name, function_args)
-
-            # action_prompt_template = PromptTemplate.from_template(action_template)
-            # action_prompt = action_prompt_template.format(user_input=user_input, function_name=function_name, action_result=action_result)
-            
-            yield LLMChat.llm_chat(action_template, user_input=user_input, function_name=function_name, action_result=action_result)
+            async for one_result in LLMChat.llm_chat(function_call_template, user_input=user_input, history=prompt_history):
+                self.final_result += one_result
+                yield one_result
         else:
-            lack_parameters = BotCheck.lack_parameters(function_args, function_name)
-            have_parameters = BotCheck.have_parameters(function_args)
 
-            # ask_user_prompt_template = PromptTemplate.from_template(ask_user_template)
-            # ask_user_prompt = ask_user_prompt_template.format(user_input=user_input, function_name=function_name, have_parameters=have_parameters, parameters=lack_parameters)
+            if BotCheck.slot_is_full(function_args, function_name):
+                logger.info("parameters is full !")
+                # 判断是否是自定义Agent
+                if Agent.check_name_iscustom(function_name):
+                    action_result = self.custom_action(function_name, function_args)
+                else:
+                    action_result = self.action(function_name, function_args)
 
-            yield LLMChat.llm_chat(ask_user_template, user_input=user_input, function_name=function_name, have_parameters=have_parameters, parameters=lack_parameters)
+                # action_prompt_template = PromptTemplate.from_template(action_template)
+                # action_prompt = action_prompt_template.format(user_input=user_input, function_name=function_name, action_result=action_result)
+                
+                async for one_result in LLMChat.llm_chat(action_template, user_input=user_input, function_name=function_name, action_result=action_result):
+                    self.final_result += one_result
+                    yield one_result
+            else:
+                lack_parameters = BotCheck.lack_parameters(function_args, function_name)
+                have_parameters = BotCheck.have_parameters(function_args)
+
+                # ask_user_prompt_template = PromptTemplate.from_template(ask_user_template)
+                # ask_user_prompt = ask_user_prompt_template.format(user_input=user_input, function_name=function_name, have_parameters=have_parameters, parameters=lack_parameters)
+                
+                async for one_result in LLMChat.llm_chat(ask_user_template, user_input=user_input, function_name=function_name, have_parameters=have_parameters, parameters=lack_parameters):
+                    self.final_result += one_result
+                    yield one_result
     
     def action(self, function_name, function_args):
         try:
