1cd3836370e67bf056d7aa2af11a6d7bfde38f5e
xtsmstd123 <139766552+xtsmstd123@users.noreply.github.com>
2024-08-29 11:44:05 +0800

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './router';
+import { createPinia } from 'pinia'
+import persistState from 'pinia-plugin-persistedstate';
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+const pinia = createPinia();
+pinia.use(persistState);
+
+app.use(router);
+app.use(pinia);
+app.mount('#app')
