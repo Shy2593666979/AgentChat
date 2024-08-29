7fae0f1c625f9d8940a54957c2e8f6bf403fe2b0
xtsmstd123 <139766552+xtsmstd123@users.noreply.github.com>
2024-08-29 11:40:24 +0800

    Create index.ts

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,48 @@
+// router/index.ts
+import { createRouter, createWebHistory } from 'vue-router';
+import ChatPage from '../pages/conversation/chatPage/chatPage.vue';
+import NotFound from '../pages/notFound/index';
+import Index from '../pages/index.vue'
+import conversation from '../pages/conversation/conversation.vue';
+import DefaultPage from '../pages/conversation/defaultPage/defaultPage.vue';
+const routes  = [
+
+  {
+    path: '/',
+    redirect:'/conversation/',
+    name: 'index',
+    component: Index,    
+    children:[
+      {
+
+        path: '/conversation',
+        name: 'conversation',
+        component: conversation,
+        children:[
+          {
+            path: '/conversation/',
+            name: 'defaultPage',
+            component: DefaultPage,
+          },
+          {
+            path: '/conversation/chatPage',
+            name: 'chatPage',
+            component: ChatPage,
+          }
+        ]
+      },
+    ]
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'not-found',
+    component: NotFound,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
