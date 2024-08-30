3b65356a56ce1c87fb5785df6658f7ff39732e82
Shy2593666979 <2593666979@qq.com>
2024-08-30 17:09:57 +0800

    Update __init__.py

--- chat/config/__init__.py
+++ chat/config/__init__.py
@@ -1,4 +1,5 @@
 import yaml
+import os
 
 # 将用户配置的LLM进行设置全局变量
 LLM_OPENAI_BASE_URL = ""
@@ -34,8 +35,16 @@ RAG_RERANK_COHERE_API_KEY = ""
 RAG_RERANK_DEFAULT_MODEL = ""
 RAG_RERANK_DEFAULT_CACHE_DIR = ""
 
-def update_system_config():
-    with open("../config/system_config.yaml", 'r', encoding='utf-8') as file:
+# 更新本地config文件
+def update_yaml_file(data):
+    data_dict = yaml.safe_load(data)
+
+    with open("config/system_config.yaml", 'w', encoding='utf-8') as file:
+        yaml.dump(data_dict, file , default_flow_style=False)
+
+# 更新config文件中的全局变量
+def update_user_config():
+    with open("config/system_config.yaml", 'r', encoding='utf-8') as file:
         config = yaml.safe_load(file)
 
     global LLM_OPENAI_API_KEY, LLM_OPENAI_BASE_URL, LLM_OPENAI_MODEL
@@ -54,3 +63,21 @@ def update_system_config():
     LANGFUSE_SECRET_KEY = config['langfuse']['secret_key']
     LANGFUSE_PUBLIC_KEY = config['langfuse']['public_key']
     LANGFUSE_HOST = config['langfuse']['host']
+
+    TOOL_WEATHER_BASE_URL = config['tool']['weather']['base_url']
+    TOOL_WEATHER_API_KEY = config['tool']['weather']['api_key']
+    TOOL_DELIVERY_BASE_URL = config['tool']['delivery']['base_url']
+    TOOL_DELIVERY_API_KEY = config['tool']['delivery']['api_key']
+    TOOL_GOOGLE_API_KEY = config['tool']['google']['api_key']
+
+    RAG_EMBEDDING_CHOOSE = config['rag']['embedding']['choose']
+    RAG_EMBEDDING_OPENAI_API_KEY = config['rag']['embedding']['openai']['api_key']
+    RAG_EMBEDDING_OPENAI_MODEL = config['rag']['embedding']['openai']['model']
+    RAG_EMBEDDING_OPENAI_BASE_URL = config['rag']['embedding']['openai']['base_url']
+    RAG_EMBEDDING_DEFAULT_CACHE_DIR = config['rag']['embedding']['default']['cache_dir']
+    RAG_EMBEDDING_DEFAULT_MODEL = config['rag']['embedding']['default']['model']
+
+    RAG_RERANK_CHOOSE = config['rag']['rerank']['choose']
+    RAG_RERANK_COHERE_API_KEY = config['rag']['rerank']['cohere']['api_key']
+    RAG_RERANK_DEFAULT_MODEL = config['rag']['rerank']['default']['model']
+    RAG_RERANK_DEFAULT_CACHE_DIR = config['rag']['rerank']['default']['cache_dir']
