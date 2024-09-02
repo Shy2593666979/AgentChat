e72a7cec9ede5064def1c5aa0970c85a647e9e7b
xtsmstd123 <139766552+xtsmstd123@users.noreply.github.com>
2024-09-02 17:50:30 +0800

    Update notFound.vue

--- frontend/src/pages/notFound/notFound.vue
+++ frontend/src/pages/notFound/notFound.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts"></script>
 
 <template>
-<h1>404</h1>
+  <div class="notFound">
+    <div>
+      <div class="text">抱歉，未找到该页面!</div>
+    <div>
+      <img src="../../assets/404.gif" alt="" width="500px" height="500px">
+    </div>
+    </div>
+  </div>
 </template>
 
 <style>
-
+.notFound{
+  height: calc(100vh);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .text{
+    display: flex;
+    font-size: 25px;
+    justify-content: center;
+    color:rgb(157, 157, 157);
+  }
+}
 </style>
