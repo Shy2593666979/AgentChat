56640d1b7ea04825fce05aa108b49ffa4affaae1
Shy2593666979 <2593666979@qq.com>
2024-09-02 15:43:18 +0800

    Update word.py

--- chat/rag/document/word.py
+++ chat/rag/document/word.py
@@ -1,15 +1,22 @@
 from docx import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter, TextSplitter
+from langchain_community.document_loaders import UnstructuredWordDocumentLoader
 
 def word_loader(path: str, text_splitter: TextSplitter):
-    # 使用 python-docx 加载 Word 文档
-    doc = Document(path)
+    loader = UnstructuredWordDocumentLoader(path)
 
-    # 读取文档内容
-    pages = [p.text for p in doc.paragraphs]
-
-    result = text_splitter.split_documents(pages)
-
+    result = loader.load_and_split(text_splitter=text_splitter)
     return result
+    
+# def word_loader(path: str, text_splitter: TextSplitter):
+#     # 使用 python-docx 加载 Word 文档
+#     doc = Document(path)
+
+#     # 读取文档内容
+#     pages = [p.text for p in doc.paragraphs]
+
+#     result = text_splitter.split_documents(pages)
+
+#     return result
 
 
