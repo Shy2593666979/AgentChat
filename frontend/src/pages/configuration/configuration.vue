bc85e2b8fa323e90cdde9bf210e02a23e94d70c3
xtsmstd123 <139766552+xtsmstd123@users.noreply.github.com>
2024-09-06 11:10:30 +0800

    Create configuration.vue

---
+++ frontend/src/pages/configuration/configuration.vue
@@ -0,0 +1,18 @@
+
+<template>
+
+</template>
+ 
+<script>
+
+
+</script>
+
+<style lang="scss" scoped>
+.agent-card {
+  padding: 20px;
+  display: grid;
+  grid-template-columns: 20% 20% 20% 20% 20%;
+
+}
+</style>
