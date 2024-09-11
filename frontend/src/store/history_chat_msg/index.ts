ced4ff2d4fff61418a67b902c29680fe5d7683f6
xtsmstd123 <139766552+xtsmstd123@users.noreply.github.com>
2024-09-11 13:58:09 +0800

    Update index.ts

--- frontend/src/store/history_chat_msg/index.ts
+++ frontend/src/store/history_chat_msg/index.ts
@@ -1,21 +1,40 @@
 import { defineStore } from 'pinia';
-import { ref } from 'vue'
+import {   ref } from 'vue'
 import { ChatMessage } from '../../type';
 import { getHistoryMsgAPI } from '../../apis/history';
-import { getHistoryChat } from '../../utils/function'
-
 
 export const useHistoryChatStore = defineStore('history_chat_msg', () => {
   const chatArr = ref<ChatMessage[]>([])
   const dialogId = ref('')
   const name = ref('')
   const logo = ref('')
-  async function HistoryChat(id: string) {
+  function HistoryChat(id: string){
     chatArr.value = []
-    const list = await getHistoryMsgAPI(JSON.parse(JSON.stringify(id as string)))
-    getHistoryChat(list, chatArr.value)
+    async function getList(id: string) {
+      const list = await getHistoryMsgAPI(JSON.parse(JSON.stringify(id as string)))
+        for (let i = 0; i < list.data.data.length; i += 2) {
+          const chatMsg = ref<ChatMessage>({
+            personMessage: { content: '' },
+            aiMessage: { content: '' }
+          })
+          if (list.data.data[i].role === 'user') {
+            chatMsg.value.personMessage.content = list.data.data[i].content
+          }
+          if (list.data.data[i + 1].role === 'user') {
+            chatMsg.value.personMessage.content = list.data.data[i + 1].content
+          }
+          if (list.data.data[i].role === 'assistant') {
+            chatMsg.value.aiMessage.content = list.data.data[i].content
+          }
+          if (list.data.data[i + 1].role === 'assistant') {
+            chatMsg.value.aiMessage.content = list.data.data[i + 1].content
+          }
+          chatArr.value.push(chatMsg.value)
+        }
+    }
+    getList(id)
   }
-  async function clear() {
+  function clear() {
     chatArr.value = []
   }
   return { chatArr, HistoryChat ,clear,dialogId,name,logo}
