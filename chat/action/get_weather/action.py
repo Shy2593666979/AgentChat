1857b5e448c8e0dddcb9219777935ab948737d0d
Shy2593666979 <2593666979@qq.com>
2024-11-19 15:07:55 +0800

    Update action.py

--- chat/action/get_weather/action.py
+++ chat/action/get_weather/action.py
@@ -1,32 +1,54 @@
+from venv import logger
+
 import requests
+from typing import Type
+from langchain.tools import BaseTool
+from pydantic import BaseModel, Field
 from config.user_config import userConfig
 from prompt.tool_prompt import WEATHER_PROMPT, MESSAGE_PROMPT
 
-def get_weather_action(location: str):
+
+class WeatherInput(BaseModel):
+    location: str = Field(description='输入输入想要查询的位置')
+
+
+class WeatherTool(BaseTool):
+    name = 'weather'
+    description = '帮助用户想要查询的天气'
+    args_schema: Type[BaseModel] = WeatherInput
+
+    def _run(self, location: str):
+            return get_weather(location)
+
+
+def get_weather(location: str):
+    """帮助用户想要查询的天气"""
     params = {
         'key': userConfig.TOOL_WEATHER_API_KEY,
         'city': location,
         'extensions': 'all'
     }
 
-    res = requests.get(url=userConfig.TOOL_WEATHER_BASE_URL, params=params) # 预报天气
+    try:
+        res = requests.get(url=userConfig.TOOL_WEATHER_BASE_URL, params=params, timeout=5)  # 预报天气
+        result = res.json()
+        city = result.get('forecasts')[0].get("city")  # 获取城市
+        message_result = []
+        data = result.get('forecasts')[0].get("casts")
+        for item in data:
+            date = item.get('date')  # 获取日期
+            day_temp = item.get('daytemp')  # 白天温度
+            night_temp = item.get('nighttemp')  # 晚上温度
+            day_weather = item.get('dayweather')  # 白天天气现象
+            night_weather = item.get('nightweather')  # 晚上天气现象
+            weather_message = MESSAGE_PROMPT.format(date, day_temp, night_temp, day_weather, night_weather)
 
-    result = res.json()
+            message_result.append(weather_message)
 
+        final_result = WEATHER_PROMPT.format(city, message_result[0], message_result[1:])
+        return final_result
+    except Exception as err:
+        logger.error(f'call weather tool appear Err: {err}')
+        return str(err)
 
-    city = result.get('forecasts')[0].get("city")  # 获取城市
-    message_result = []
-    data = result.get('forecasts')[0].get("casts")
-    for item in data:
-        date = item.get('date')  # 获取日期
-        day_temp = item.get('daytemp')  # 白天温度
-        night_temp = item.get('nighttemp')  # 晚上温度
-        day_weather = item.get('dayweather')  # 白天天气现象
-        night_weather = item.get('nightweather')  # 晚上天气现象
-        weather_message = MESSAGE_PROMPT.format(date, day_temp, night_temp, day_weather, night_weather)
-
-        message_result.append(weather_message)
-
-    final_result = WEATHER_PROMPT.format(city, message_result[0], message_result[1:])
-    return final_result
 
