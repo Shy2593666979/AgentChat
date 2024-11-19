bfc43c6d408c9411b35ca70beb28add8608b679c
Shy2593666979 <2593666979@qq.com>
2024-11-19 15:08:30 +0800

    Update action.py

--- chat/action/arxiv/action.py
+++ chat/action/arxiv/action.py
@@ -1,7 +1,24 @@
+from typing import Type, Any
+from langchain.tools import BaseTool
+from pydantic import BaseModel, Field
 from langchain_community.utilities import ArxivAPIWrapper
 
 arxiv_wrapper = ArxivAPIWrapper()
 
-def arxiv_action(information: str):
-    docs = arxiv_wrapper.run(information)
+class ArxivInput(BaseModel):
+    query: str = Field(description='用户输入的问题')
+
+
+class ArxivTool(BaseTool):
+    name = 'arxiv'
+    description = '为用户提供Arxiv上的论文'
+    args_schema: Type[BaseModel] = ArxivInput
+
+    def _run(self, query: str):
+        return get_arxiv(query)
+
+# 支持Function Call的模型
+def get_arxiv(query: str):
+    """为用户提供Arxiv上的论文"""
+    docs = arxiv_wrapper.run(query)
     return docs
