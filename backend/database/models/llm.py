139352e8cd2b62276576359a40dbd2a1722630ab
Shy2593666979 <2593666979@qq.com>
2024-11-20 11:33:55 +0800

    add llm model fields

--- backend/database/models/llm.py
+++ backend/database/models/llm.py
@@ -1,6 +1,8 @@
 from sqlmodel import SQLModel
 from pydantic import Field
+from datetime import datetime
 from uuid import uuid4
+import pytz
 
 class LLMTable(SQLModel, table=True):
     llm_id: str = Field(default_factory=lambda :uuid4().hex, description='大模型的ID')
@@ -8,5 +10,7 @@ class LLMTable(SQLModel, table=True):
     base_url: str = Field(description='大模型的base url')
     api_key: str = Field(description='大模型的api key')
     provider: str = Field(description='大模型的提供商')
+    user_id: str = Field(description='大模型创建者的ID')
+    create_time: datetime = Field(default_factory=lambda: datetime.now(pytz.timezone('Asia/Shanghai')))
 
 
