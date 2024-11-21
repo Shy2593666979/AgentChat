23a191a4bbbdb895a03522ce47056f24129fe778
Shy2593666979 <2593666979@qq.com>
2024-11-21 16:30:17 +0800

    Update __init__.py

--- backend/tools/__init__.py
+++ backend/tools/__init__.py
@@ -3,6 +3,7 @@ from google_search.action import google_search, GoogleSearchTool
 from arxiv.action import get_arxiv, ArxivTool
 from get_weather.action import get_weather, WeatherTool
 from delivery.action import get_delivery, DeliveryTool
+from crawl_web.action import crawl_web, CrawlWebTool
 from rag_data.action import exec_rag
 
 action_Function_call = {
@@ -11,13 +12,15 @@ action_Function_call = {
     "get_arxiv": get_arxiv,
     "get_weather": get_weather,
     "get_delivery": get_delivery,
+    "crawl_web": crawl_web,
     "RagAgent": exec_rag
 }
 
 action_React = {
     "send_email": SendEmailTool,
-    "search": GoogleSearchTool,
-    "weather": WeatherTool,
-    "delivery": DeliveryTool,
-    "arxiv": ArxivTool
+    "google_search": GoogleSearchTool,
+    "get_weather": WeatherTool,
+    "get_delivery": DeliveryTool,
+    "get_arxiv": ArxivTool,
+    "crawl_web": CrawlWebTool
 }
