e168d8f9102843f69d9513a564c7c10c6f1a332f
Shy2593666979 <2593666979@qq.com>
2024-11-22 09:52:48 +0800

    Update llm.py

--- backend/routers/llm.py
+++ backend/routers/llm.py
@@ -1,27 +1,27 @@
 from typing import Optional
 
 from fastapi import APIRouter, Depends, Body
-from scipy.ndimage import uniform_filter
-
 from service.user import get_login_user, UserPayload
-from service.llm import LLMService
 from type.schemas import UnifiedResponseModel
+
+from service.llm import LLMService
 
 router = APIRouter()
 
 @router.post('/llm/create', response_model=UnifiedResponseModel)
 async def create_llm(model: str = Body(description='大模型的名称'),
-               api_key: str = Body(description='大模型的key'),
-               base_url: str = Body(description='大模型的url'),
-               provider: str = Body(description='大模型的提供商'),
-               login_user: UserPayload = Depends(get_login_user)):
+                     api_key: str = Body(description='大模型的key'),
+                     base_url: str = Body(description='大模型的url'),
+                     llm_type: str = Body(description='模型的种类，LLM、Embedding？'),
+                     provider: str = Body(description='大模型的提供商'),
+                     login_user: UserPayload = Depends(get_login_user)):
 
     return LLMService.create_llm(model=model, api_key=api_key, base_url=base_url,
-                                 user_id=login_user.user_id, provider=provider)
+                                 user_id=login_user.user_id, provider=provider, llm_type=llm_type)
 
 @router.delete('/llm/delete', response_model=UnifiedResponseModel)
 async def delete_llm(llm_id: str = Body(description='大模型的ID'),
-               login_user: UserPayload = Depends(get_login_user)):
+                     login_user: UserPayload = Depends(get_login_user)):
 
     return LLMService.delete_llm(llm_id=llm_id, user_id=login_user.user_id)
 
@@ -30,11 +30,12 @@ async def update_llm(model: Optional[str] = Body(description='大模型的名称
                      api_key: Optional[str] = Body(description='大模型的key'),
                      base_url: Optional[str] = Body(description='大模型的url'),
                      provider: Optional[str] = Body(description='大模型的提供商'),
+                     llm_type: Optional[str] = Body(description='模型的类型'),
                      llm_id: str = Body(description='大模型的ID'),
                      login_user: UserPayload = Depends(get_login_user)):
 
     return LLMService.update_llm(user_id=login_user.user_id, model=model, api_key=api_key,
-                                 llm_id=llm_id, provider=provider, base_url=base_url)
+                                 llm_id=llm_id, provider=provider, base_url=base_url, llm_type=llm_type)
 
 @router.get('/llm/all', response_model=UnifiedResponseModel)
 async def get_all_llm(login_user: UserPayload = Depends(get_login_user)):
