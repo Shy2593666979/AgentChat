2c63694a0faed4555c896ce7e04ac6eb977692a9
Shy2593666979 <2593666979@qq.com>
2024-11-22 10:18:41 +0800

    Update dialog.py

--- backend/database/models/dialog.py
+++ backend/database/models/dialog.py
@@ -8,4 +8,5 @@ class DialogTable(SQLModel, table=True):
     dialog_id: str = Field(default_factory=lambda: uuid4().hex, primary_key=True)
     name: str = Field(description='对话绑定的Agent的名称')
     agent_id: str = Field(description='对话Dialog绑定Agent的ID')
+    user_id: str = Field(description='对话Dialog的用户ID')
     create_time: datetime = Field(default_factory=lambda: datetime.now(pytz.timezone('Asia/Shanghai')))
