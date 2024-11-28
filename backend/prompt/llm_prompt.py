249fc0cbf76476d0256de9108f06e52f9ef5a1ba
Shy2593666979 <2593666979@qq.com>
2024-11-28 17:52:12 +0800

    Update llm_prompt.py

--- backend/prompt/llm_prompt.py
+++ backend/prompt/llm_prompt.py
@@ -59,6 +59,25 @@ History: {history}
 Thought:{agent_scratchpad}
 """
 
+PROMPT_REACT_BASE = """Answer the following questions as best you can. You have access to the following APIs:
+
+{tools_text}
+
+Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tools_name_text}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can be repeated zero or more times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question
+
+Begin!
+
+Question: {query}"""
+
 agent_guide_word = "我现在知道你想要创建一个Agent帮你解决一些事情，那么我们开始吧"
 
 auto_build_ask_prompt = """
