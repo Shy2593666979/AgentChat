979d8666af4377f618338c3ce0f4fc7cd64eef95
Mingguang.Tian <2593666979@qq.com>
2026-03-08 17:49:31 +0800

    fix: 去除注册页面底部的留白和系统设置

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,30 +1,28 @@
-import { defineConfig } from 'vite'
+import { loadEnv, defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 8090,
-    // 是否开启 https
-    https: false,
-      // 设置反向代理，跨域
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    server: {
+      host: '0.0.0.0',
+      port: 8090,
+      https: false,
       proxy: {
         '/api': {
-          target: 'http://localhost:7860/',
+          target: env.VITE_API_BASE_URL || 'http://localhost:7860',
           changeOrigin: true,
-      }
+        }
+      },
     },
-  },
-  plugins: [vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  
+    plugins: [
+      vue(),
+      AutoImport({ resolvers: [ElementPlusResolver()] }),
+      Components({ resolvers: [ElementPlusResolver()] }),
+    ],
+  }
 })
